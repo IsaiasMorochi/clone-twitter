d6c23fded37ad5c7315f48435f403c5feef19980
IsaiasMorochi <isaiasmorochi95@gmail.com>
2021-04-18 11:52:49 -0400

    Modified model user.go because not permitted space in json:"value,value"

--- models/users.go
+++ models/users.go
@@ -9,14 +9,14 @@ import (
 /*Model of Users*/
 type Users struct {
 	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
-	Name      string             `bson:"name" json:"name, omitempty"`
-	LastName  string             `bson:"lastname" json:"lastname, omitempty"`
-	Birthday  time.Time          `bson:"birthday" json:"birthday, omitempty"`
-	Email     string             `bson:"email" json:"email, omitempty"`
-	Password  string             `bson:"password" json:"password, omitempty"`
-	Avatar    string             `bson:"avatar" json:"avatar, omitempty"`
-	Banner    string             `bson:"banner" json:"banner, omitempty"`
-	Biography string             `bson:"biography" json:"biography, omitempty"`
-	Location  string             `bson:"location" json:"location, omitempty"`
-	WebSite   string             `bson:"website" json:"website, omitempty"`
+	Name      string             `bson:"name" json:"name,omitempty"`
+	LastName  string             `bson:"lastname" json:"lastname,omitempty"`
+	Birthday  time.Time          `bson:"birthday" json:"birthday,omitempty"`
+	Email     string             `bson:"email" json:"email,omitempty"`
+	Password  string             `bson:"password" json:"password,omitempty"`
+	Avatar    string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner    string             `bson:"banner" json:"banner,omitempty"`
+	Biography string             `bson:"biography" json:"biography,omitempty"`
+	Location  string             `bson:"location" json:"location,omitempty"`
+	WebSite   string             `bson:"website" json:"website,omitempty"`
 }
